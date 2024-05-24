f067209879c2b4123a2ea83a66781ecd27068694
ParisiBR <parisi18@unifesp.br>
2024-05-24 17:36:38 -0300

    TodoViewSet

--- setup/apis/views.py
+++ setup/apis/views.py
@@ -1,24 +1,27 @@
-from django.shortcuts import render
-from rest_framework import generics
-from .serializers import *
-from .models import *
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from todo import models
 
-# Get
-class ListTodo(generics.ListAPIView):
-    queryset = ToDoList.objects.all()
-    serializer_class = ToDoListSerializer
+# # Get
+# class ListTodo(generics.ListAPIView):
+#     queryset = models.Todo.objects.all()
+#     serializer_class = ToDoListSerializer
 
-# Update
-class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
-    queryset = ToDoList.objects.all()
-    serializer_class = ToDoListSerializer
+# # Update
+# class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = models.Todo.objects.all()
+#     serializer_class = ToDoListSerializer
 
-# Create
-class CreateTodo(generics.CreateAPIView):
-    queryset = ToDoList.objects.all()
-    serializers_class = ToDoListSerializer
+# # Create
+# class CreateTodo(generics.CreateAPIView):
+#     queryset = models.Todo.objects.all()
+#     serializers_class = ToDoListSerializer
 
-# Delete
-class DeleteTodo(generics.DestroyAPIView): 
-    queryset = ToDoList.objects.all()
-    serializers_class = ToDoListSerializer
+# # Delete
+# class DeleteTodo(generics.DestroyAPIView): 
+#     queryset = models.Todo.objects.all()
+#     serializers_class = ToDoListSerializer
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = models.Todo.objects.all()
+    serializer_class = TodoSerializer
