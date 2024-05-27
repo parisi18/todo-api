f4e01847349623585e159760543f3b59e7d46e28
ParisiBR <parisi18@unifesp.br>
2024-05-27 14:27:18 -0300

    Todo Model

--- setup/todo/models.py
+++ setup/todo/models.py
@@ -2,8 +2,18 @@ from django.db import models
 
 
 class Todo(models.Model):
+
+    STATUS_CHOICES = (
+        ('A', 'A fazer'),
+        ('F', 'Fazendo'),
+        ('P', 'Pronto')
+    )
+
     title = models.CharField(max_length=200)
     description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_lenght=1, choices=STATUS_CHOICES, blank=False, null=False, default='A')
+    completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
