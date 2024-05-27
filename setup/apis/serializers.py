fbeaeabb832767a8fa3beccf65b9a1731c457cbc
ParisiBR <parisi18@unifesp.br>
2024-05-27 14:14:42 -0300

    TodoSerializer refactor

--- setup/apis/serializers.py
+++ setup/apis/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
-from todo import models
+from todo.models import Todo
 
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.Todo
-        fields = ('id', 'title', 'description')
+        model = Todo
+        fields = '__all__'
 
