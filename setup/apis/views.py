c5017bc576d301e206522156260da98c87da1d73
ParisiBR <parisi18@unifesp.br>
2024-05-27 15:34:10 -0300

    Basic auth

--- setup/apis/views.py
+++ setup/apis/views.py
@@ -1,8 +1,12 @@
 from rest_framework import viewsets
 from .serializers import TodoSerializer
 from todo.models import ToDo
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class TodoViewSet(viewsets.ModelViewSet):
     """A simple ViewSet for viewing and editing accounts."""
     queryset = ToDo.objects.all()
     serializer_class = TodoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
