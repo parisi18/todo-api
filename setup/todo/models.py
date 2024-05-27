d6755229984b92c5bcd2309e33b58b2ddbfd720d
ParisiBR <parisi18@unifesp.br>
2024-05-27 14:58:15 -0300

    max_length fix

--- setup/todo/models.py
+++ setup/todo/models.py
@@ -12,7 +12,7 @@ class Todo(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_lenght=1, choices=STATUS_CHOICES, blank=False, null=False, default='A')
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, blank=False, null=False, default='A')
     completed = models.BooleanField(default=False)
 
     def __str__(self):
