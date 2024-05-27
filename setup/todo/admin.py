d2898e6692a6fa960b59b2d1394ec2c187c1b9ef
ParisiBR <parisi18@unifesp.br>
2024-05-27 14:26:55 -0300

    Admin TodoList

--- setup/todo/admin.py
+++ setup/todo/admin.py
@@ -3,3 +3,10 @@ from django.contrib import admin
 from .models import Todo
 
 admin.site.register(Todo)
+
+class ToDoListAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'date', 'completed')
+    list_filter = ('completed', 'date')
+    search_fields = ('title', 'description')
+    list_per_page = 25
+    ordering = ['completed', 'date']
