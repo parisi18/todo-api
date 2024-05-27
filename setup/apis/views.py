93e7b9421711c7de7e56b3fe1ec9395944e9b6fa
ParisiBR <parisi18@unifesp.br>
2024-05-27 14:15:37 -0300

    Todo import refactor

--- setup/apis/views.py
+++ setup/apis/views.py
@@ -1,27 +1,8 @@
 from rest_framework import viewsets
 from .serializers import TodoSerializer
-from todo import models
-
-# # Get
-# class ListTodo(generics.ListAPIView):
-#     queryset = models.Todo.objects.all()
-#     serializer_class = ToDoListSerializer
-
-# # Update
-# class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = models.Todo.objects.all()
-#     serializer_class = ToDoListSerializer
-
-# # Create
-# class CreateTodo(generics.CreateAPIView):
-#     queryset = models.Todo.objects.all()
-#     serializers_class = ToDoListSerializer
-
-# # Delete
-# class DeleteTodo(generics.DestroyAPIView): 
-#     queryset = models.Todo.objects.all()
-#     serializers_class = ToDoListSerializer
+from todo.models import Todo
 
 class TodoViewSet(viewsets.ModelViewSet):
-    queryset = models.Todo.objects.all()
+    """A simple ViewSet for viewing and editing accounts."""
+    queryset = Todo.objects.all()
     serializer_class = TodoSerializer
